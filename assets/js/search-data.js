708060cb4ae29334764309119f02ecb4ed51747d
thunderous77 <thunderous0377@gmail.com>
2025-08-05 09:22:18 +0000

    Deploying to gh-pages from @ thunderous77/thunderous77.github.io@7d7017752774531982208a3f3dcd2c493f2e59c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -377,14 +377,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-paper-glape-gold-label-agnostic-prompt-evaluation-for-large-language-models-has-been-accepted-to-emnlp-2024-sparkles",
           title: 'Paper “GLaPE: Gold Label-agnostic Prompt Evaluation for Large Language Models” has been accepted...',
           description: "",
